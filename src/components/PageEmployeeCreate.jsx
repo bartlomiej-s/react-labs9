1dfe76beefadc6d43b814149bdefa3497f9a92d0
Bartek <bartek77@live.com>
2020-01-03 09:54:13 +0100

    Task1.1

--- src/components/PageEmployeeCreate.jsx
+++ src/components/PageEmployeeCreate.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { withRouter } from 'react-router-dom'
 import { connect } from 'react-redux'
 
 class PageEmployeeCreate extends React.Component {
@@ -95,5 +96,5 @@ class PageEmployeeCreate extends React.Component {
   }
 }
 
-export default connect(
-)(PageEmployeeCreate)
+export default withRouter(connect(
+)(PageEmployeeCreate))
